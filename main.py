d3462df7a57cf8fe81950e5da31fdc95041e4cc3
yangsu <2506806016@qq.com>
2025-10-02 18:09:08 +0800

    feat: 添加 D3D11 和 IUnknown 类的实现，支持窗口截图功能

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: BugNotFound
+# @Date: 2025-10-02 14:45:20
+# @LastEditTime: 2025-10-02 14:45:23
+# @FilePath: /DeltaForceGun/main.py
+# @Description: 
+
+
+
+if __name__ == "__main__":
+    print("Hello, DeltaForceGun!")
